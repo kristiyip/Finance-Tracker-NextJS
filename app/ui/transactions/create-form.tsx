4b917c61810f4a4d4b00e3accae70eac5deae706
Kristin Diep <kristiyip@gmail.com>
2024-10-23 13:23:29 -0400

    add name attribute to form inputs for server actions

--- app/ui/transactions/create-form.tsx
+++ app/ui/transactions/create-form.tsx
@@ -2,6 +2,7 @@
 
 import { useActionState, useState, useEffect } from "react"
 import { createTransaction, State } from "@/app/lib/actions"
+import { Button } from "../button";
 
 export default function CreateTransactionForm() {
   const [title, setTitle] = useState('')
@@ -12,11 +13,13 @@ export default function CreateTransactionForm() {
   const [state, formAction] = useActionState(createTransaction, initState)
 
   return (
-    <form>
+    <form action={formAction}>
       <div>
         <div>
           <label>Title</label>
           <input 
+            id="title"
+            name="title"
             className="text-black"
             value={title} 
             onChange={e => setTitle(e.target.value)}
@@ -25,6 +28,8 @@ export default function CreateTransactionForm() {
         <div>
           <label>Amount</label>
           <input
+            id="amount"
+            name="amount"
             className="text-black"
             value={amount}
             onChange={e => setAmount(e.target.value)}
@@ -33,12 +38,15 @@ export default function CreateTransactionForm() {
         <div>
           <label>Description</label>
           <input
+            id="description"
+            name="description"
             className="text-black"
             value={description}
             onChange={e => setDescription(e.target.value)}
           />
         </div>
       </div>
+      <Button type="submit">Add Transaction</Button>
     </form>
   )
 }
